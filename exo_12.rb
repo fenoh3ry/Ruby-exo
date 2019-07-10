68630e85f3d08c22e0139b1967b340880810ef8f
fenohery.fanomezanirina@gmail.com <fenohery.fanomezanirina@gmail.com>
2019-07-10 10:25:06 +0300

    add_exo_12_13

--- exo_12.rb
+++ exo_12.rb
@@ -1,7 +1,7 @@
 puts "Donne moi un nombre, peu importe"
 nombre = gets.chomp
-a = nombre.to_i
+f = nombre.to_i
 
-a.times do 
-	puts "Salut, ça farte"
+for count in (0..f)
+	puts count
 end
---
+++ exo_13.rb
@@ -0,0 +1,9 @@
+puts "Donnez moi votre année de naissance"
+
+y = gets.chomp
+x = y.to_i
+
+for count in (x..2019)
+
+puts count
+end
