9f013309ec0b15e8a46c0efe7882f9fdf7af3b2f
fenohery.fanomezanirina@gmail.com <fenohery.fanomezanirina@gmail.com>
2019-07-10 14:57:26 +0300

    exo-15-16

--- exo_15.rb
+++ exo_15.rb
@@ -1,11 +1,12 @@
-puts " Omeo ny taona nahaterahanao"
+puts " Omeo ny taona nahaterahanao ? "
 taona = gets.chomp.to_i
-i=0
-m = 0
-for i in (taona..2019)
-	puts "amin'ny taona #{i} ,dia #{m} taona ianao "
 
-	i += 1
-	m += 1
+
+test = 0
+for hahaha in (taona..2019)
+	puts "amin'ny taona #{hahaha} ,dia #{test} taona ianao "
+
+	hahaha += 1
+	test += 1
 
 end 
---
+++ exo_16.rb
@@ -0,0 +1,13 @@
+puts "Entrer votre age"
+a = gets.chomp.to_i
+	y = 2019 - a
+
+
+	for i 	in 0..a
+
+	puts "En #{y} il y a #{a} ans, tu as #{i} ans "
+
+		i += 1
+		a -= 1
+		y += 1
+	end
